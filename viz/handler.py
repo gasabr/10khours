830ea24bedf4538e7ac611512c09beae39af1c30
Gleb Abroskin <abroskingleb@gmail.com>
2016-11-28 00:11:10 +0300

    Site can plot a bar and put it in a folder. Commit is all about data representation.

--- viz/handler.py
+++ viz/handler.py
@@ -1,13 +1,31 @@
 import os
 import httplib2
 import calendar
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from datetime import datetime, timedelta, date
+import calendar as calendar_module
 from django.conf import settings
 from apiclient import discovery
-from .models import CalendarModel, PeriodModel
+from .models import CalendarModel
 from gauth.models import CredentialsModel
 from django.contrib.auth.models import User
 
+PERIODS = ['this_month', 'this_week', 'today']
+
+
+def get_duration(event):
+    """
+    event is { 'start': {'dataTime' : datetime.datetime(),},
+               'end': {'dataTime' : datetime.datetime(),},
+               ...
+    returns difference in hours
+    """
+    end = datetime.strptime(event['end']['dateTime'], '%Y-%m-%dT%H:%M:%S+03:00')
+    start = datetime.strptime(event['start']['dateTime'], '%Y-%m-%dT%H:%M:%S+03:00')
+    return float((end-start).seconds / 60) / 60
 
 class Handler():
     """
@@ -44,19 +62,96 @@ class Handler():
                                                       user=user, 
                                                       name=c['summary'])                                      
                 new_cm.save()
+        
+                
+    def create_periods(self, periods):
+        P = []
+        now   = datetime.now()
+        start = datetime.utcnow()
+        end   = datetime.utcnow()
+        delta = timedelta(days=1)
+
+        for period_type in periods:
+            if period_type == 'this_month':
+                start = datetime.combine(date(now.year, now.month, 1), 
+                                         datetime.min.time())
+                # How to make it readable?
+                # obtaining last day of the month
+                end   = datetime.combine(date(now.year, now.month, calendar_module.monthrange(now.year, now.month)[1]),
+                                         datetime.min.time())
+                delta = timedelta(days=1)
+    
+            elif period_type == 'today':
+                start = datetime.combine(start, datetime.min.time())
+                end   = datetime.combine(start, datetime.max.time())
+                delta = timedelta(hours=1)
+    
+            elif period_type == 'this_week':
+                start = datetime.combine(date(now.year, now.month, now.day) - timedelta(days=now.weekday()),
+                                         datetime.min.time())
+                # obtaining last day of the week
+                end   = start + timedelta(days=6)
+                delta = timedelta(days=1)
+                
+                
+            bounds = []
+            tmp = start
+            while tmp < end:
+                bounds.append(tmp)
+                tmp += delta
+            bounds.append(end)
+                
+            P.append({'type'  : period_type,
+                      'start' : start,
+                      'end'   : end,
+                      'delta' : delta,
+                      'bounds': bounds}
+                    )
+        return P
+                                
+    def get_bounds(self, periods, period_type):
+        for p in periods:
+            if p['type'] == period_type:
+                return (p['bounds'], p['delta'])
+        return ()
     
 
-    def get_events(self, calendars_ids, start_time, end_time):
+    def fetch_events(self, calendars_ids, periods):
         """
         takes array of calendars ids and time interval
-        returns {'calendar_id': [events_id,...,]}
+        returns {'calendar_id': [events,...,]}
         """
-        events = {}
+        events = []
         for c_id in calendars_ids:
-            events[c_id] = self.service.events().list(calendarId=c_id,
-                                                      timeMax=end_time,
-                                                      timeMin=start_time).execute()
+            for period in periods:
+                timeMax = period['end'].isoformat()+'Z'
+                timeMin = period['start'].isoformat()+'Z'
+                items   = self.service.events().list(calendarId=c_id,
+                                                     timeMax=timeMax,
+                                                     timeMin=timeMin
+                                                    ).execute().get('items', [])
+                                                  
+                events.append({'calendarId' : c_id,
+                               'period_type': period['type'],
+                               'items'      : items}
+                            )
 
+        return events
+        
+    
+    def spread_events(self, events, periods):
+        X = []
+        for e in events:
+            e['items_in_periods'] = {}
+            bounds, delta = self.get_bounds(periods, e['period_type'])
+            # X = [map(str, x) for x in bounds]
+            for i in e['items']:
+                period_start = bounds[0]
+                event_start = datetime.strptime(i['start']['dateTime'], '%Y-%m-%dT%H:%M:%S+03:00')
+                while period_start < event_start:
+                    period_start += delta
+                i[e['period_type']] = period_start
+                
         return events
 
 
@@ -67,23 +162,35 @@ class Handler():
         return [(x.id, x.name) for x in CalendarModel.objects.all().filter(user__username=self.user.username)]
 
 
-    def create_graphs(self, calendars_list, periods_type):
+    def create_graphs(self, calendars, periods):
         """
         takes what to show on graphs in 2 lists
         plots it with matplolib
         saves images in /BASE_DIR/<username> 
         """
-        # all events stored by calendar_id
-        events = {}
+        # prepare data
+        P = self.create_periods(periods)
+        events_series = self.spread_events(self.fetch_events(calendars, P), P)
+        
         # create folder to store images
-        folder  = os.path.join(settings.BASE_DIR, 'graphs/'+self.user.username)
+        folder  = os.path.join(settings.BASE_DIR, 'graphs/'+self.user.username+'/')
         if not os.path.exists(folder):
             os.makedirs(folder)
-
-        for calendar_id in calendars_list:
-            for period_type in periods_type:
-                PeriodModel.objects.create_period(calendar_id, period_type)
+        
+        tmp_period = periods[0]
+        D = []
+        for index, bound in enumerate(P[0]['bounds']):
+            if index >= len(D):
+                D.append(0)
+            for e in events_series[0]['items']:
+                if e[P[0]['type']] == bound:
+                    D[index] += get_duration(e)
+                    
+        image_path = folder+'bar.png'    
             
+        plt.bar([l for l, _ in enumerate(D)], height=D)
+        plt.grid(True)
+        plt.savefig(image_path)
 
-        return (0, 0, 0)
+        return [image_path]
 
